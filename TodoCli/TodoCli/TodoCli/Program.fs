b8681134bf04a6729b0c9b64b0b360109cab5a62
Matt Shanahan <matt.shanahan@relativity.com>
2020-02-04 22:09:11 -0600

    Initial commit for TodoCli

---
+++ TodoCli/TodoCli/TodoCli/Program.fs
@@ -0,0 +1,25 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let square x = x * x
+
+type Todo = { description: string; notes: string; status: string }
+
+let nTodo d n s = { description = d; status = s; notes = n }
+
+let todos = [
+    nTodo "Get groceries"       "TODO"        "Bananas, foobars, beef"
+    nTodo "Fix bike"            "IN PROGRESS" ""
+    nTodo "Do some other thing" "COMPLETE"    "Good job!"
+]
+
+//let buildTable ts =
+//    let maxDesc = List.maxBy 
+//    let header = 
+
+[<EntryPoint>]
+let main argv =
+    printfn "TODO        STATUS         NOTES"
+    printfn "%d squared is %d!" 12 (square 12)
+    0 // return an integer exit code
